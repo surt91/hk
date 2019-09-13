bfa0fede28ab994bb2fe9f4fdfa3e83316925657
Hendrik Schawe <surt91@gmail.com>
2019-09-13 11:03:26 +0200

    added basic benchmark capability

---
+++ benches/benchmark.rs
@@ -0,0 +1,20 @@
+#[macro_use]
+extern crate criterion;
+
+use criterion::Criterion;
+use criterion::black_box;
+
+extern crate hk;
+use hk::HegselmannKrause;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut hk = HegselmannKrause::new(10, 13);
+    c.bench_function("hk N=10 sweep", |b| b.iter(|| hk.sweep()));
+    let mut hk = HegselmannKrause::new(100, 13);
+    c.bench_function("hk N=100 sweep", |b| b.iter(|| hk.sweep()));
+    let mut hk = HegselmannKrause::new(1000, 13);
+    c.bench_function("hk N=1000 sweep", |b| b.iter(|| hk.sweep()));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
