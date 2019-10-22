4af4f18a1df01166871fea1b86a440a6e581cf1f
Hendrik Schawe <surt91@gmail.com>
2019-10-22 09:49:10 +0200

    updated the map python evaluation script

--- py/make_eps_range_map.py
+++ py/make_eps_range_map.py
@@ -2,8 +2,12 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
+import gzip
+
 import parameters
 from parameters import parameters as ps
+
+matplotlib.rc('text', usetex=True)
 
 xs = []
 ys = []
@@ -13,30 +17,61 @@ for p in ps:
     el = p["tolerance_lower"]
     eu = p["tolerance_upper"]
     name = parameters.outname.format(**p).replace(".", "")
-    try:
-        b = np.loadtxt(name + ".cluster.dat.gz")
-    except:
-        print("file not found:", name + ".cluster.dat.gz")
-        continue
+    with gzip.open(name + ".cluster.dat.gz") as f:
+        for line in f:
+            if line.startswith(b"#"):
+                continue
+            a = list(map(float, line.split()))
 
-    for a in b:
-        try:
-            len(a)
-        except:
-            val = a
-        else:
-            val = max(a)
-        xs.append(el)
-        ys.append(eu)
-        zs.append(val)
+            try:
+                len(a)
+            except:
+                val = a
+            else:
+                val = max(a)
+            xs.append(el)
+            ys.append(eu)
+            zs.append(val/p["num_agents"]/p["samples"])
 
-plt.hist2d(
+xsort = sorted(set(xs))
+xbins = np.linspace(xsort[0]-(xsort[1]-xsort[0])/2, xsort[-1]+(xsort[-1]-xsort[-2])/2, len(set(xs))+1, endpoint=True)
+ysort = sorted(set(ys))
+ybins = np.linspace(ysort[0]-(ysort[1]-ysort[0])/2, ysort[-1]+(ysort[-1]-ysort[-2])/2, len(set(ys))+1, endpoint=True)
+
+import json
+import lzma
+
+data = {
+    "x": list(xs),
+    "y": list(ys),
+    "weight": list(zs),
+    "xbins": list(xbins),
+    "ybins": list(ybins),
+}
+
+with lzma.open("data.json.xz", "w") as f:
+    f.write(json.dumps(data).encode('utf-8'))
+
+fig = plt.gcf()
+fig.set_size_inches(3+3/8, 2.5)
+
+plt.xlabel("$\\varepsilon_1$")
+plt.ylabel("$\\varepsilon_2$")
+
+h, xedges, yedges, _ = plt.hist2d(
     xs,
     ys,
     weights=zs,
-    bins=(len(set(xs)), len(set(ys))),
+    bins=(xbins, ybins),
     cmap=plt.cm.viridis_r,
-    norm=matplotlib.colors.LogNorm(),
-    density=True
+    vmin=0,
+    vmax=1,
+#    norm=matplotlib.colors.LogNorm(),
+#    density=True
 )
-plt.show()
+plt.colorbar()
+plt.tight_layout()
+
+fig.savefig('map.pdf', dpi=300)
+fig.savefig("map.png")
+#plt.show()
