9d5c08fd4c5f9b81ff070176b3e82a8312e540f1
Hendrik Schawe <surt91@gmail.com>
2020-05-18 16:13:48 +0200

    benchmarks work again

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -2,32 +2,25 @@
 extern crate criterion;
 
 use criterion::Criterion;
-use criterion::black_box;
 
 extern crate hk;
 use hk::HegselmannKrauseBuilder;
-use hk::{Model, anneal, Linear};
+use hk::PopulationModel;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    // let mut hk = HegselmannKrause::new(1000, 0., 1., 13);
-    // c.bench_function("hk N=10 sweep", |b| b.iter(|| hk.sweep()));
-    // let mut hk = HegselmannKrause::new(100, 0., 1., 13);
-    // c.bench_function("hk N=100 sweep", |b| b.iter(|| hk.sweep()));
-    let mut hk = HegselmannKrauseBuilder::new(100, 0., 1.).seed(13).build();
+    let mut hk = HegselmannKrauseBuilder::new(100)
+        .seed(13)
+        .population_model(PopulationModel::Uniform(0., 1.))
+        .build();
+
+    hk.reset();
     c.bench_function("hk N=1000 sweep", |b| b.iter(|| hk.sweep()));
 
-    let mut hk = HegselmannKrauseBuilder::new(1000, 0., 1.).seed(13).build();
+    hk.reset();
     c.bench_function("hk N=1000 sync sweep", |b| b.iter(|| hk.sweep_synchronous_naive()));
 
-    let mut hk = HegselmannKrauseBuilder::new(1000, 0., 1.).seed(13).build();
+    hk.reset();
     c.bench_function("hk N=1000 sync btree sweep", |b| b.iter(|| hk.sweep_synchronous_bisect()));
-
-
-    use rand::SeedableRng;
-    use rand_pcg::Pcg64;
-    let mut hk = HegselmannKrauseBuilder::new(100, 0., 1.).seed(13).eta(0.5).build();
-    let mut rng = Pcg64::seed_from_u64(42);
-    c.bench_function("hk annealing", |b| b.iter(|| anneal(&mut hk, Linear::new(20, 0.), &mut rng)));
 }
 
 criterion_group!(benches, criterion_benchmark);
